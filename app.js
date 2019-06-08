774b182f68308b5e1e44ccd7006ddb1fe5eb88e1
Keunhak Lim <limkeunhak@gmail.com>
2019-06-09 02:05:31 +0900

    Test logging code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-let dateFormat = require('dateFormat');
+let dateFormat = require('dateformat');
 
 var productsRouter = require('./routes/products');
 
